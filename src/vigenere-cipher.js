882d67d2254ff30445e28b8ccae351dfb0c91c75
Rostislav <rost.tm@gmail.com>
2025-11-08 10:04:42 +0300

    feat: solved 20 problems

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,19 +20,61 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+    constructor(type = true) {
+        this.isDirect = type;
+    }
 
-  decrypt() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
+    _process(message, key, mode) {
+        if (message === undefined || key === undefined) {
+            throw new Error('Incorrect arguments!');
+        }
+
+        message = String(message);
+        key = String(key).toUpperCase();
+
+        let result = '';
+        let keyIndex = 0;
+
+        for (let i = 0; i < message.length; i++) {
+            const char = message[i].toUpperCase();
+            const code = char.charCodeAt(0);
+
+            if (code >= 65 && code <= 90) {
+                const keyChar = key[keyIndex % key.length];
+                const keyCode = keyChar.charCodeAt(0) - 65;
+                let newCode;
+
+                if (mode === 'encrypt') {
+                    newCode = ((code - 65 + keyCode) % 26) + 65;
+                } else {
+                    newCode = ((code - 65 - keyCode + 26) % 26) + 65;
+                }
+
+                result += String.fromCharCode(newCode);
+                keyIndex++;
+            } else {
+                result += char;
+            }
+        }
+
+        if (!this.isDirect) {
+            result = result.split('').reverse().join('');
+        }
+
+        return result;
+    }
+
+    encrypt(message, key) {
+        return this._process(message, key, 'encrypt');
+    }
+
+    decrypt(encryptedMessage, key) {
+        return this._process(encryptedMessage, key, 'decrypt');
+    }
 }
 
 module.exports = {
-  directMachine: new VigenereCipheringMachine(),
-  reverseMachine: new VigenereCipheringMachine(false),
-  VigenereCipheringMachine,
+    directMachine: new VigenereCipheringMachine(),
+    reverseMachine: new VigenereCipheringMachine(false),
+    VigenereCipheringMachine,
 };

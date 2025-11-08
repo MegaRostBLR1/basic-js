914b9069f595d4d163413a874a5d637c5dcacca6
Rostislav <rost.tm@gmail.com>
2025-11-08 11:01:39 +0300

    fix: fix tasks problems

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,40 +1,55 @@
 const { NotImplementedError } = require('../lib');
 
+/**
+ * Create transformed array based on the control sequences that original
+ * array contains
+ *
+ * @param {Array} arr initial array
+ * @returns {Array} transformed array
+ *
+ * @example
+ *
+ * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
+ * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
+ *
+ */
+
 function transform(arr) {
     if (!Array.isArray(arr)) {
         throw new Error("'arr' parameter must be an instance of the Array!");
     }
 
     const res = [];
-    const controls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
 
     for (let i = 0; i < arr.length; i++) {
-        const current = arr[i];
-
-        if (!controls.includes(current)) {
-            res.push(current);
-            continue;
-        }
-
-        if (current === '--discard-next') {
-            i++;
-        } else if (current === '--discard-prev') {
-            if (res.length > 0) {
-                res.pop();
-            }
-        } else if (current === '--double-next') {
-            if (i + 1 < arr.length && !controls.includes(arr[i + 1])) {
-                res.push(arr[i + 1]);
-            }
-        } else if (current === '--double-prev') {
-            if (i > 0 && !controls.includes(arr[i - 1])) {
-                res.push(arr[i - 1]);
-            }
+        switch (arr[i]) {
+            case "--discard-next":
+                i++;
+                break;
+            case "--discard-prev":
+                if (i > 0 && arr[i - 2] !== "--discard-next") {
+                    res.pop();
+                }
+                break;
+            case "--double-next":
+                if (i < arr.length - 1) {
+                    res.push(arr[i + 1]);
+                }
+                break;
+            case "--double-prev":
+                if (i > 0 && arr[i - 2] !== "--discard-next") {
+                    res.push(arr[i - 1]);
+                }
+                break;
+            default:
+                res.push(arr[i]);
+                break;
         }
     }
 
     return res;
 }
+
 
 module.exports = {
     transform
